b396f29f21b9ced721550bd38f9435b955b0f118
Pied-Piper46 <kbdylan1@gmail.com>
2025-06-06 00:15:08 +0900

    add basePath for github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/shadowing-card', // Replace with your repository name
   /* config options here */
 };
 
